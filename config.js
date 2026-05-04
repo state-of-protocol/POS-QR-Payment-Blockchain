9ac70d9f2d3bdf9d07d113c7be530bb2d7dd2096
ShiftDev.js <35767816+state-of-protocol@users.noreply.github.com>
2026-05-05 00:20:22 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * config.js - Tetapan Vendor
+ */
+const VENDOR_SETTINGS = {
+    wallet_address: "lum13fgcln9k7ft2pkndjh4xhzlv3y3l8e450vnztl",
+    conversion_rate: 17000, // 1 RM = 17,000 LUM
+    default_myr: 10,
+    currency_prefix: "lum"
+};
